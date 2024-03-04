ecbd88869baecec15fd37d9b0ca1afa2e7078f9b
Mathew <126032483+MathewAnderson6706@users.noreply.github.com>
2024-03-03 22:28:22 -0500

    -Mathew Fragment works -Added textview and radio buttons -AlertDialog added

--- MathewModule7/src/main/java/mathew/anderson/n01436706/MathewFragment.java
+++ MathewModule7/src/main/java/mathew/anderson/n01436706/MathewFragment.java
@@ -1,5 +1,7 @@
 package mathew.anderson.n01436706;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +9,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MathewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MathewFragment extends Fragment {
+
+    RadioButton radioButton1, radioButton2, radioButton3, radioButton4;
+    RadioGroup radioGroup;
 
     public MathewFragment() {
         // Required empty public constructor
@@ -23,6 +25,50 @@ public class MathewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mathew, container, false);
+        View view = inflater.inflate(R.layout.fragment_mathew, container, false);
+
+        radioButton1 = view.findViewById(R.id.mathewRadioButton1);
+        radioButton2 = view.findViewById(R.id.mathewRadioButton2);
+        radioButton3 = view.findViewById(R.id.mathewRadioButton3);
+        radioGroup = view.findViewById(R.id.mathewRadioGroup);
+
+        // Set the first radio button as selected by default
+        radioButton1.setChecked(true);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton selectedRadioButton = group.findViewById(checkedId);
+                String selectedLabel = selectedRadioButton.getText().toString();
+                showAlertDialog(selectedLabel);
+            }
+        });
+
+        return view;
+    }
+
+    private void showAlertDialog(String selectedLabel) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+
+        builder.setIcon(R.mipmap.ic_new_image);
+
+        builder.setTitle("Mathew Anderson");
+
+        builder.setMessage(selectedLabel);
+
+        // Set OK button
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Dismiss the dialog
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setCancelable(false);
     }
 }
