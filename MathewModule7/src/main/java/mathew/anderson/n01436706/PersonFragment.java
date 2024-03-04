d7fd8fc5dde02a67b6a7488334deea26f4f2538f
Mathew <126032483+MathewAnderson6706@users.noreply.github.com>
2024-03-03 22:46:05 -0500

    -Person Fragment almost working -Have the list up and populated -Not able to send data over yet

--- MathewModule7/src/main/java/mathew/anderson/n01436706/PersonFragment.java
+++ MathewModule7/src/main/java/mathew/anderson/n01436706/PersonFragment.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +18,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class PersonFragment extends Fragment {
+
+    private ListView listView;
+
+    String[] locations;
+
 
     public PersonFragment() {
         // Required empty public constructor
@@ -24,6 +32,26 @@ public class PersonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_person, container, false);
+        View view = inflater.inflate(R.layout.fragment_person, container, false);
+
+        locations = getResources().getStringArray(R.array.provinces_and_territories);
+        listView = view.findViewById(R.id.mathewListView1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, locations);
+        //bind the list view with array adapter
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override public void onItemClick(AdapterView<?> arg0, View arg1,int position, long arg3)
+            {
+                listView.setSelector(android.R.color.holo_blue_dark);
+                // Pass selected concept and definition to the second fragment
+                String selectedConcept = locations[position];
+            }
+        });
+
+        return view;
     }
 }
