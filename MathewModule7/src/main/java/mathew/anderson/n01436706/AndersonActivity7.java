f615925fd97f8d718d288a8f459ee0571dc925e0
Mathew <126032483+MathewAnderson6706@users.noreply.github.com>
2024-03-03 21:04:10 -0500

    Nav Menu -can switch between fragments

--- MathewModule7/src/main/java/mathew/anderson/n01436706/AndersonActivity7.java
+++ MathewModule7/src/main/java/mathew/anderson/n01436706/AndersonActivity7.java
@@ -1,14 +1,58 @@
 package mathew.anderson.n01436706;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class AndersonActivity7 extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class AndersonActivity7 extends AppCompatActivity implements NavigationBarView
+.OnItemSelectedListener{
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView
+                = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView
+                .setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.person);
+    }
+    FirstFragment firstFragment = new FirstFragment();
+    SecondFragment secondFragment = new SecondFragment();
+    ThirdFragment thirdFragment = new ThirdFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.person) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, firstFragment)
+                    .commit();
+            return true;
+        } else if (itemId == R.id.home) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, secondFragment)
+                    .commit();
+            return true;
+        } else if (itemId == R.id.settings) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, thirdFragment)
+                    .commit();
+            return true;
+        }
+
+        return false;
     }
 }
